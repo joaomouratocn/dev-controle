c75fe4a2f3b4a4e0bffb9413ef41596b30acaa09
joao-mourato <joaomouratocn@gmail.com>
2025-08-06 14:53:35 -0300

    criado container para centralizar e pagina de dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/container";
+
+export default function Dashboard() {
+  return (
+    <main>
+      <Container>
+        <h1>Dashboard</h1>
+      </Container>
+    </main>
+  );
+}
