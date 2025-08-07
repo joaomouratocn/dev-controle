8507a2894ce28c9c49b64301be6c8a7cbc131960
joao-mourato <joaomouratocn@gmail.com>
2025-08-07 16:49:54 -0300

    continuar autenticação google

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
     globalWithPrisma.prisma = new PrismaClient();
   }
 
-  prisma = globalWithPrisma;
+  prisma = globalWithPrisma.prisma;
 }
 
 export default prisma;
